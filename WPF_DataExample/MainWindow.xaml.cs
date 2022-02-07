8e431642df7c1c5cd9379bc5e10b68e1c397c027
Sebastian Belz <sbelz192@gmail.com>
2022-02-07 10:47:55 +0100

    Update: Hinzufügen von Mitarbeitern mit Auswahlmöglichkeiten etc.Updated GUI, Button-Events

--- WPF_DataExample/MainWindow.xaml.cs
+++ WPF_DataExample/MainWindow.xaml.cs
@@ -41,7 +41,7 @@ namespace WPF_DataExample
         /// Creates a database connection, selects * from tabletype and fills the main datagrid with the selection.
         /// </summary>
         /// <param name="tabletype">the table name that will fill the datagrid</param>
-        private void LoadDatabaseTableCommand(Tabletype tabletype)
+        private void FillDataGrid(Tabletype tabletype, DataGrid DG)
         {
             try
             {
@@ -52,11 +52,11 @@ namespace WPF_DataExample
 
                     using (MySqlCommand cmd = conn.CreateCommand())
                     {
-                        cmd.CommandText = "select * from " + tabletype + ";";
+                        cmd.CommandText = "SELECT * FROM " + tabletype + ";";
                         DataTable dt = new DataTable();
                         MySqlDataAdapter da = new MySqlDataAdapter(cmd);
                         da.Fill(dt);
-                        dataGrid.ItemsSource = dt.DefaultView;
+                        DG.ItemsSource = dt.DefaultView;
                     }
 
                     conn.Close();
@@ -67,8 +67,11 @@ namespace WPF_DataExample
                 MessageBox.Show(ex.ToString());
             }
         }
-
-        public void DeleteSelectedMitarbeiter(UInt32 mid)
+        /// <summary>
+        /// Deletes the selected employee identified by 'm_id' from the database.
+        /// </summary>
+        /// <param name="m_id"></param>
+        public void DeleteSelectedMitarbeiter(UInt32 m_id)
         {
             try
             {
@@ -79,7 +82,7 @@ namespace WPF_DataExample
 
                     using (MySqlCommand cmd = conn.CreateCommand())
                     {
-                        cmd.CommandText = "DELETE FROM mitarbeiter WHERE m_id =" + mid + ";";
+                        cmd.CommandText = "DELETE FROM mitarbeiter WHERE m_id =" + m_id + ";";
                         Debug.WriteLine(cmd.CommandText);
                         Debug.WriteLine("MySQL table mitarbeiter: " + cmd.ExecuteNonQuery() + " rows affected!");
                     }
@@ -91,25 +94,37 @@ namespace WPF_DataExample
             {
                 MessageBox.Show(ex.ToString());
             }
-            LoadDatabaseTableCommand(Tabletype.Mitarbeiter);
+            FillDataGrid(Tabletype.Mitarbeiter, dataGrid);
         }
 
         private void MenuItem_Mitarbeiter_Click(object sender, RoutedEventArgs e)
         {
-            LoadDatabaseTableCommand(Tabletype.Mitarbeiter);
+            dataGrid.Visibility = Visibility.Visible;
+            dataGridNeu.Visibility = Visibility.Hidden;
             saveNewMitarbeiter.Visibility = Visibility.Hidden;
+            SP_Mitarbeiter_Neu_Felder.Visibility = Visibility.Hidden;
+            SP_Mitarbeiter_Neu_Labels.Visibility = Visibility.Hidden;
+            FillDataGrid(Tabletype.Mitarbeiter, dataGrid);
         }
 
         private void MenuItem_Beruf_Click(object sender, RoutedEventArgs e)
         {
-            LoadDatabaseTableCommand(Tabletype.Beruf);
+            dataGrid.Visibility = Visibility.Visible;
+            dataGridNeu.Visibility = Visibility.Hidden;
             saveNewMitarbeiter.Visibility = Visibility.Hidden;
+            SP_Mitarbeiter_Neu_Felder.Visibility = Visibility.Hidden;
+            SP_Mitarbeiter_Neu_Labels.Visibility = Visibility.Hidden;
+            FillDataGrid(Tabletype.Beruf, dataGrid);
         }
 
         private void MenuItem_Abteilung_Click(object sender, RoutedEventArgs e)
         {
-            LoadDatabaseTableCommand(Tabletype.Abteilung);
+            dataGrid.Visibility = Visibility.Visible;
+            dataGridNeu.Visibility = Visibility.Hidden;
             saveNewMitarbeiter.Visibility = Visibility.Hidden;
+            SP_Mitarbeiter_Neu_Felder.Visibility = Visibility.Hidden;
+            SP_Mitarbeiter_Neu_Labels.Visibility = Visibility.Hidden;
+            FillDataGrid(Tabletype.Abteilung, dataGrid);
 
         }
 
@@ -120,69 +135,156 @@ namespace WPF_DataExample
 
         private void MenuItem_MitarbeiterNeu_Click(object sender, RoutedEventArgs e)
         {
-            dataGrid.ItemsSource = new List<Mitarbeiter>() { };
-            dataGrid.CanUserAddRows = true;
+            dataGrid.Visibility = Visibility.Hidden;
+            dataGridNeu.Visibility = Visibility.Visible;
             saveNewMitarbeiter.Visibility = Visibility.Visible;
-        }
+            SP_Mitarbeiter_Neu_Felder.Visibility = Visibility.Visible;
+            SP_Mitarbeiter_Neu_Labels.Visibility = Visibility.Visible;
 
-        private void SaveNewMitarbeiter_Click(object sender, RoutedEventArgs e)
-        {
-            if (ValidateNewMitarbeiter())
+            // Comboboxen füllen:
+            string connstr = "Server=127.0.0.1;Port=3306;Uid=root;Pwd=;database=mv2";
+            using (MySqlConnection conn = new MySqlConnection(connstr))
             {
-                string connstr = "Server=127.0.0.1;Port=3306;Uid=root;Pwd=;database=mv2";
-                using (MySqlConnection conn = new MySqlConnection(connstr))
-                {
-                    conn.Open();
+                conn.Open();
 
-                    using (MySqlCommand cmd = conn.CreateCommand())
+                // Abteilung:
+                using (MySqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT * FROM abteilung WHERE abteilungsleiter IS NOT NULL;";
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
                     {
-                        foreach (Mitarbeiter item in dataGrid.Items.OfType<Mitarbeiter>().ToList())
+                        while (reader.Read())
                         {
-                            cmd.CommandText = "insert into mitarbeiter (vorname,nachname,gehalt) values(" + MitarbeiterToSQLString(item) + ");";
-                            Debug.WriteLine(cmd.CommandText);
-                            Debug.WriteLine("MySQL table mitarbeiter: " + cmd.ExecuteNonQuery() + " rows affected!");
+                            CB_Abteilung.Items.Add(ReadSingleRow(reader));
                         }
                     }
-                    conn.Close();
+
                 }
-                LoadDatabaseTableCommand(Tabletype.Mitarbeiter);
-                saveNewMitarbeiter.Visibility = Visibility.Hidden;
-                dataGrid.CanUserAddRows = false;
+
+                // Beruf:
+                using (MySqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT b_id,bezeichnung FROM beruf;";
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            CB_Beruf.Items.Add(ReadSingleRow(reader));
+                        }
+                    }
+                }
+
+                // Vorgesetzter:
+                List<UInt32> vorgesetzte_ids = new List<UInt32>();
+                using (MySqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT abteilungsleiter FROM abteilung WHERE abteilungsleiter IS NOT NULL;";
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            vorgesetzte_ids.Add(ReadSingleRow2(reader));
+                        }
+                    }
+                }
+                using (MySqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT m_id,vorname,nachname FROM mitarbeiter;";
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            if (vorgesetzte_ids.Contains(UInt32.Parse(ReadSingleRow3(reader)[0])))
+                            {
+                                CB_Vorgesetzter.Items.Add(string.Join(' ', ReadSingleRow3(reader)));
+                            }
+                        }
+                    }
+                }
+                conn.Close();
             }
+            
+        }
+        public string ReadSingleRow(IDataRecord dataRecord)
+        {
+            return String.Format("{0} {1}", dataRecord[0], dataRecord[1]);
+        }
+        public UInt32 ReadSingleRow2(IDataRecord dataRecord)
+        {
+            return (UInt32)dataRecord[0];
+        }
+        public List<string> ReadSingleRow3(IDataRecord dataRecord)
+        {
+            return new List<string>() { dataRecord[0].ToString(), (string)dataRecord[1], (string)dataRecord[2]};
+        }
+        private void SaveNewMitarbeiter_Click(object sender, RoutedEventArgs e)
+        {
+            string connstr = "Server=127.0.0.1;Port=3306;Uid=root;Pwd=;database=mv2";
+            using (MySqlConnection conn = new MySqlConnection(connstr))
+            {
+                conn.Open();
+
+                using (MySqlCommand cmd = conn.CreateCommand())
+                {
+                    Mitarbeiter m = new Mitarbeiter()
+                    {
+                        vorname = TB_Vorname.Text,
+                        nachname = TB_Nachname.Text,
+                        geb_dat = TB_Geburtsdatum.Text,
+                        gehalt = TB_Gehalt.Text,
+                        vorgesetzter_id = UInt32.Parse(CB_Vorgesetzter.SelectedValue.ToString().Split(' ')[0]),
+                        a_id = UInt32.Parse(CB_Abteilung.SelectedValue.ToString().Split(' ')[0]),
+                        b_id = UInt32.Parse(CB_Beruf.SelectedValue.ToString().Split(' ')[0]),
+ 
+                    };
+                    cmd.CommandText = "INSERT INTO mitarbeiter (vorname,nachname,geb_dat,gehalt,vorgesetzter_id,a_id,b_id) VALUES(" + MitarbeiterToCSVString(m) + ");";
+                    cmd.ExecuteNonQuery();
+                }
+                conn.Close();
+            }
+            FillDataGrid(Tabletype.Mitarbeiter, dataGridNeu);
+        }
+        /// <summary>
+        /// Deletes the selected employee identified by 'm_id' from the database.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void delete_Click(object sender, RoutedEventArgs e)
+        {
+            int i = dataGrid.SelectedIndex;
+            DataRowView v = (DataRowView)dataGrid.Items[i];
+            UInt32 s = (UInt32)v[0];
+            DeleteSelectedMitarbeiter(s);
+        }
+
+        //TODO: Validierung fertig entwickeln!
+        public bool ValidateMitarbeiter(Mitarbeiter M)
+        {
+            if(M.gehalt.Length == 0) return false;
+            if(M.vorname.Length == 0) return false;
+            if(M.nachname.Length == 0) return false;
+
+            return true;
         }
 
         /// <summary>
-        /// Makes sure, that the given User data is safe and fits the table requirements.
+        /// Converts an employee object into a format for SQL insert statement values (in between brackets, separated with commas,...).
         /// </summary>
+        /// <param name="m"></param>
         /// <returns></returns>
-        public bool ValidateNewMitarbeiter()
+        public string MitarbeiterToCSVString(Mitarbeiter m)
         {
-            Window popup = new Window();
-            foreach (var item in dataGrid.Items.OfType<Mitarbeiter>())
+            List<string> s = new List<string>
             {
-                if (item.vorname.Length == 0)
-                {
-                    popup.Content = "invalid \"vorname\"";
-                    popup.ShowDialog();
-                    return false;
-
-                }
-                else if (item.nachname.Length == 0)
-                {
-                    popup.Content = "invalid \"nachname\"";
-                    popup.ShowDialog();
-                    return false;
-
-                }
-                else if (item.gehalt.Length == 0)
-                {
-                    popup.Content = "invalid \"gehalt\"";
-                    popup.ShowDialog();
-                    return false;
-
-                }
-            }
-            return true;
+                "\'" + m.vorname + "\'",
+                "\'" + m.nachname + "\'",
+                "\'" + m.geb_dat + "\'",
+                "\'" + m.gehalt + "\'",
+                "\'" + m.vorgesetzter_id + "\'",
+                "\'" + m.a_id + "\'",
+                "\'" + m.b_id + "\'"
+            };
+            return string.Join(',', s);
         }
 
         /// <summary>
@@ -190,15 +292,18 @@ namespace WPF_DataExample
         /// </summary>
         public class Mitarbeiter
         {
-            //public string mid { get; set; }
+            public UInt32 m_id { get; set; }
             public string vorname { get; set; }
             public string nachname { get; set; }
-            //public string gebdatum { get; set; }
+            public string geb_dat { get; set; }
             public string gehalt { get; set; }
-            //public string vorgesetzterid { get; set; }
-            // public string aid { get; set; }
-            // public string bid { get; set; }
+            public string beruf { get; set; }
+            public string abteilung { get; set; }
+            public UInt32 vorgesetzter_id { get; set; }
+            public UInt32 a_id { get; set; }
+            public UInt32 b_id { get; set; }
         }
+
         public enum Tabletype
         {
             Mitarbeiter,
@@ -210,28 +315,5 @@ namespace WPF_DataExample
 
         }
 
-        /// <summary>
-        /// Converts an employee object into a format for SQL insert statement values (in between brackets, separated with commas,...).
-        /// </summary>
-        /// <param name="m"></param>
-        /// <returns></returns>
-        public string MitarbeiterToSQLString(Mitarbeiter m)
-        {
-            List<string> s = new List<string>();
-            s.Add("\'" + m.vorname + "\'");
-            s.Add("\'" + m.nachname + "\'");
-
-            s.Add("\'" + m.gehalt + "\'");
-            return string.Join(',', s);
-        }
-
-        private void delete_Click(object sender, RoutedEventArgs e)
-        {
-            int i = dataGrid.SelectedIndex;
-            DataRowView v = (DataRowView)dataGrid.Items[i];
-            UInt32 s = (UInt32)v[0];
-            DeleteSelectedMitarbeiter(s);
-            
-        }
     }
 }
